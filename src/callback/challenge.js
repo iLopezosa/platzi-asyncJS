77850cf21d44d2251bbd2de429b1c2ab7d9f7f79
Ignacio Lopezosa Serrano <n4xo@pm.me>
2022-11-20 00:26:59 +0100

    fetchData

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -1,4 +1,4 @@
-const XMLHttpRequest = require('xmlhttprequest');
+const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 const API = 'https://api.escuelajs.co/api/v1'
 
 function fetchData(urlApi, callback) {
@@ -9,13 +9,22 @@ function fetchData(urlApi, callback) {
         if (xhttp.readyState === 4) {
             if(xhttp.status === 200) {
                 callback(null, JSON.parse(xhttp.responseText));
+            } else {
+                const error = new Error(`ERROR ${urlApi}`);
+                return callback(error, null);
             }
-        } else {
-            const error = new Error(`ERROR ${urlApi}`);
-            return callback(error, null);
         }
     }
     xhttp.send()
 }
 
-fetchData(API, console.log)
+fetchData(`${API}/products`, (error, products) => {
+    if (error) return console.error(error);
+    fetchData(`${API}/products/${products[0].id}`, (error, product) => {
+        if(error) return console.error(error);
+        fetchData(`${API}/categories/${product?.category?.id}`, (error, category) => {
+            if (error) return console.error(error);
+            console.log(products[0], '\n', product.title, '\n', category.name);
+        })
+    })
+})
