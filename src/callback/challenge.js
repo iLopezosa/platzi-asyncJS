249011ad5ca3a930dd7e56de93e03e2ab86d4e4d
Ignacio Lopezosa Serrano <n4xo@pm.me>
2022-11-20 00:10:45 +0100

    XMLHttpRequest

---
+++ src/callback/challenge.js
@@ -0,0 +1,21 @@
+const XMLHttpRequest = require('xmlhttprequest');
+const API = 'https://api.escuelajs.co/api/v1'
+
+function fetchData(urlApi, callback) {
+    let xhttp = new XMLHttpRequest();
+
+    xhttp.open('GET', urlApi, true);
+    xhttp.onreadystatechange = function (event) {
+        if (xhttp.readyState === 4) {
+            if(xhttp.status === 200) {
+                callback(null, JSON.parse(xhttp.responseText));
+            }
+        } else {
+            const error = new Error(`ERROR ${urlApi}`);
+            return callback(error, null);
+        }
+    }
+    xhttp.send()
+}
+
+fetchData(API, console.log)
